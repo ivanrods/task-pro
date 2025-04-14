80301622aa3a5a499e5360013e64b4e8d124289c
Ivan <contaivanrodrigues@gmail.com>
2025-04-14 11:43:06 -0300

    feat: add functions in task details

--- src/app/task/[task]/page.tsx
+++ src/app/task/[task]/page.tsx
@@ -2,18 +2,26 @@
 import { useParams } from "next/navigation";
 import { useTask } from "../../context/TaskContext";
 import { Circle, Star } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function TaskDetail() {
+  const router = useRouter();
   const { task } = useParams<{ task: string }>();
-  const { tasks } = useTask();
+  const { tasks, toggleCompleted, toggleFavorite, deleteTask } = useTask();
   const taskDetail = tasks.find((t) => t.id === task);
-  console.log(tasks);
+
+  const [title, setTitle] = useState(taskDetail?.title || "");
+  const [description, setDescription] = useState(taskDetail?.description || "");
+
+  console.log(taskDetail);
   return (
     <form>
       <fieldset className="w-full flex justify-center flex-col gap-6 md:px-6">
-        <legend className="text-2xl font-bold mb-4">{taskDetail?.title}</legend>
+        <legend className="text-2xl font-bold mb-4">{title}</legend>
         <input
           type="text"
-          value={taskDetail?.title}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           className="bg-white px-4 py-4 rounded-lg text-blue-500 border-none outline-none"
         />
         <textarea
@@ -21,7 +29,8 @@ export default function TaskDetail() {
           id=""
           placeholder="Adicione uma descrição"
           className="bg-white px-4 py-4 rounded-lg text-blue-500 border-none outline-none h-52"
-          value={taskDetail?.description}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
         <input
           type="date"
@@ -30,19 +39,44 @@ export default function TaskDetail() {
           className="bg-white px-4 py-4 rounded-lg text-blue-500 border-none outline-none"
         />
         <div className="w-full flex flex-col gap-4 text-blue-500">
-          <button className="flex gap-2 ">
-            <Circle /> concluído
+          <button
+            type="button"
+            onClick={() => taskDetail && toggleCompleted(taskDetail.id)}
+            className="flex gap-2 "
+          >
+            <Circle fill={taskDetail?.completed ? "#3b82f6" : "none"} />{" "}
+            concluído
           </button>
-          <button className="flex gap-2 ">
-            <Star /> Favoritar
+          <button
+            type="button"
+            onClick={() => taskDetail && toggleFavorite(taskDetail.id)}
+            className="flex gap-2 "
+          >
+            <Star fill={taskDetail?.favorite ? "#3b82f6" : "none"} /> Favoritar
           </button>
         </div>
 
         <div className="w-full flex flex-col md:flex-row items-center justify-center gap-4">
-          <button className="w-full bg-blue-500 text-white py-2 rounded-md cursor-pointer">
+          <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            
+            console.log('Salvar clicado');
+          }}
+          className="w-full bg-blue-500 text-white py-2 rounded-md cursor-pointer">
             Salvar
           </button>
-          <button className="w-full text-blue-500 border py-2 rounded-md cursor-pointer">
+          <button
+            type="button"
+            onClick={() => {
+              if (taskDetail) {
+                deleteTask(taskDetail.id);
+                router.back();
+              }
+            }}
+            className="w-full text-blue-500 border py-2 rounded-md cursor-pointer"
+          >
             Excluir
           </button>
         </div>
