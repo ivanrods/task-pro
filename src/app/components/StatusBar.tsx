262fde9594bdacd193d2b6c2612eaeb2065c648f
Ivan <contaivanrodrigues@gmail.com>
2025-07-22 16:45:11 -0300

    feat: add change in layout status bar

--- src/app/components/StatusBar.tsx
+++ src/app/components/StatusBar.tsx
@@ -1,23 +1,24 @@
 "use client";
+import { MessageSquareText } from "lucide-react";
 import { useStatusBar } from "../context/StatusBarContext";
-
 
 const StatusBar = () => {
   const { statusBar } = useStatusBar();
   if (!statusBar) return null;
 
-  const bgColorClass =
+  const colorStatus =
     {
-      blue: "bg-[var(--primary-color)]",
-      red: "bg-[var(--alert-color)]",
-    }[statusBar.color] || "bg-[var(--primary-color)]";
+      blue: "[var(--primary-color)]",
+      red: "[var(--alert-color)]",
+    }[statusBar.color] || "[var(--primary-color)]";
 
   return (
     <p
-      className={`${bgColorClass} z-40 text-white font-semibold text-sm text-center w-full py-1 absolute top-0 transition-transform duration-2000 ease-in-out transform shadow-lg ${
-        statusBar ? "translate-y-0" : "-translate-y-full"
+      className={`bg-[var(--background-secondary)] flex flex-row gap-2 items-center z-40 text-${colorStatus} text-sm px-3 py-0.5 text-center absolute top-4 right-4 border border-${colorStatus} rounded-full ${
+        statusBar ? "animate-bounce" : "animate-none"
       }`}
     >
+      <MessageSquareText size={20} className={`${colorStatus}`} />
       {statusBar.title}
     </p>
   );
